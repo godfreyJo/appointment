77c634ed0b1ad9da20c8e1cbc98efd41dd39a2b7
godfreyJo <godfreyoyugi@gmail.com>
2020-06-02 04:20:09 +0300

    setting up for Heroku upload

--- DenWeb/DenWeb/settings.py
+++ DenWeb/DenWeb/settings.py
@@ -124,6 +124,24 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+'''
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'godfreyoyugi@gmail.com'
+EMAIL_HOST_PASSWORD = '@1988Godyme2'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+'''
+#  python -m smtpd -n -c DebuggingServer localhost:1025
+EMAIL_HOST = 'localhost'
+EMAIL_PORT =  1025
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
+# EMAIL_USE_SSL = False
+
+
+
 # TEMPLATE_DIRS = (
 #     os.path.join(BASE_DIR, 'templates'),
 # )
