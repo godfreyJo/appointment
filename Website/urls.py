bc68d5f3ada3cfb039ff2677250ac155b0f5adbc
godfreyJo <godfreyoyugi@gmail.com>
2020-06-04 05:34:34 +0300

    adding the other  html views into the urls path in the website app

--- Website/urls.py
+++ Website/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [ 
     path('', views.home, name="home"),
     path('contact.html', views.contact, name="contact"),
+    path('pricing.html', views.pricing, name="pricing"),
+    path('about.html', views.about, name="about"),
+    path('service.html', views.service, name="service"),
+    path('blog.html', views.blog, name="blog"),
     
 ]
