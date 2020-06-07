2faa472f931a36f4eb3d594869fc2d76142fc505
godfreyJo <godfreyoyugi@gmail.com>
2020-06-07 14:44:01 +0300

    adding the bookings fields to the app

--- Website/models.py
+++ Website/models.py
@@ -2,4 +2,9 @@ from django.db import models
 from datetime import datetime
 
 class Booking(models.Model):
-    
+    Fname = models.CharField(max_length=30)
+    Lname = models.CharField(max_length=30)
+    emailAddress = models.EmailField(max_length=30)
+    phoneNumber = models.CharField(max_length=20)
+    date = models.DateTimeField(auto_now_add=False, auto_now=False, blank=True)
+    timeStamp = models.DateTimeField(auto_now_add=True, auto_now=False, blank=True)
