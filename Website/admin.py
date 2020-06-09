2be397a0ccbdb9a659b8b49d81d5a3557ff11e94
godfreyJo <godfreyoyugi@gmail.com>
2020-06-09 19:25:30 +0300

    Adding the missng date field

--- Website/admin.py
+++ Website/admin.py
@@ -4,10 +4,10 @@ from .models import Booking
 from .forms import BookingForm
 
 class BookingAdmin(admin.ModelAdmin):
-    list_display = ['Fname', 'Lname', 'emailAddress', 'phoneNumber', 'booking_date', 'timeStamp']
+    list_display = ['Fname', 'Lname', 'emailAddress', 'phoneNumber', 'date', 'timeStamp','your_message']
     form = BookingForm
-    list_filter = ['Fname', 'Lname', 'emailAddress', 'phoneNumber', 'booking_date', 'timeStamp']
-    search_fields = ['Fname', 'emailAddress', 'phoneNumber', 'booking_date', ]
+    list_filter = ['Fname', 'Lname', 'emailAddress', 'phoneNumber', 'date', 'timeStamp','your_message']
+    search_fields = ['Fname', 'emailAddress', 'phoneNumber', 'date', ]
 
 
 admin.site.register(Booking, BookingAdmin)
