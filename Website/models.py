ed4e7134f53455d1a009c894fa0014d564333bf9
godfreyJo <godfreyoyugi@gmail.com>
2020-06-09 19:27:39 +0300

    adding the your_message filed in the models page

--- Website/models.py
+++ Website/models.py
@@ -6,9 +6,9 @@ class Booking(models.Model):
     Lname = models.CharField(max_length=30)
     emailAddress = models.EmailField(max_length=30)
     phoneNumber = models.CharField(max_length=20)
-    booking_date = models.DateTimeField(auto_now_add=False, auto_now=False, blank=True, null=True)
+    date = models.DateTimeField(auto_now_add=False, auto_now=False, blank=True, null=True)
     timeStamp = models.DateTimeField(auto_now_add=False, auto_now=False, blank=True)
-
+    your_message = models.TextField(max_length=200)
     def __str__(self):
         return self.Fname
 
